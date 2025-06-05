411897a59ff7a72c5f3e942cb301396aae7ca07a
kateyvk <131284242+kateyvk@users.noreply.github.com>
2025-06-05 13:45:26 -0400

    Main Menu scene
    making a menu/title scene to transition into the game

---
+++ FishGame/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  // For scene loading
+
+public class MainMenuUI : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject optionsPanel;
+    public GameObject aboutPanel;
+    
+    
+
+    public void StartGame()
+    {
+        // Load the scene named "GameScene" (change this to the name of your actual gameplay scene)
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ShowOptions()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void ShowAbout()
+    {
+        mainMenuPanel.SetActive(false);
+        aboutPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        optionsPanel.SetActive(false);
+        aboutPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        // Close the game (only works in builds)
+        Application.Quit();
+        Debug.Log("Quit Game"); // This is just for logging in the editor
+    }
+}
